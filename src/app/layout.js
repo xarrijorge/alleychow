10594dbb6a4b26073d0e307d103009cc37612e5c
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-27 23:43:08 +0000

    Add loader to the root element

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import tailwindConfig from "../../tailwind.config";
-import './globals.css'
-import Footer from './Footer'
+import "./globals.css";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div id="loader"></div>
         {children}
+
         <Footer />
       </body>
     </html>
