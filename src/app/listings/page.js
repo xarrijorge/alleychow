50674bc41d378895a63d4e6bbce8a08ca139cc2a
Xarri Jorge <kodinglife@gmail.com>
2024-01-27 18:58:06 +0000

    Added Listings Page

---
+++ src/app/listings/page.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import SingleCard from '../Components/Card'
+
+const Listings = () => {
+  return <>
+    <section className="bg-gray-2 pb-10 pt-20 dark:bg-dark lg:pb-20 lg:pt-[120px]">
+      <div className="container">
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          <SingleCard
+            image="https://i.ibb.co/r2zns1m/image-01.jpg"
+            CardTitle="50+ Best creative website themes & templates"
+            titleHref="/#"
+            btnHref="/#"
+            CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
+            Button="View Details"
+          />
+          <SingleCard
+            image="https://i.ibb.co/0nbbWM9/image-02-1.jpg"
+            CardTitle="Creative Card Component designs graphic elements"
+            CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
+            Button="View Details"
+          />
+          <SingleCard
+            image="https://i.ibb.co/dL9fH7N/image-03-1.jpg"
+            CardTitle="The ultimate UX and UI guide to card design"
+            CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
+            Button="View Details"
+          />
+        </div>
+      </div>
+    </section>
+  </>
+
+}
+
+export default Listings
+
