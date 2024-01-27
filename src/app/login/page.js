44e06bd2c60dc50b9710a2ace569bdb90ed4a32b
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-27 23:11:33 +0000

    Correct the styling for login button

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -73,11 +73,12 @@ export default function SignInPage() {
                   Forgot password?
                 </a>
               </div>
+
               <button
                 type="submit"
-                className="w-full text-white bg-orange-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full flex items-center justify-center text-white bg-orange-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                <FaGoogle /> Login with google
+                <FaGoogle className="w-6 h-6 mr-2" /> Login with Google
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
