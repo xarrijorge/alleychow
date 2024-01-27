3ad448129771ed6245a94868971e4c8226addf7b
Xarri Jorge <kodinglife@gmail.com>
2024-01-27 20:01:10 +0000

    Centered children

--- src/app/listings/page.js
+++ src/app/listings/page.js
@@ -2,7 +2,8 @@ import React from 'react'
 import SingleCard from '../Components/Card'
 
 const Listings = () => {
-  return <>
+  return <div class="w-screen flex flex-col justify-center items-center">
+    <h1 className="text-2xl font-bold mt-8 mt:mb-0 md:text-7xl">Current Businesses</h1>
     <section className="bg-gray-2 pb-10 pt-20 dark:bg-dark lg:pb-20 lg:pt-[120px]">
       <div className="container">
         <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
@@ -10,7 +11,7 @@ const Listings = () => {
             image="https://i.ibb.co/r2zns1m/image-01.jpg"
             CardTitle="50+ Best creative website themes & templates"
             titleHref="/#"
-            btnHref="/#"
+            btnHref="5"
             CardDescription="Lorem ipsum dolor sit amet pretium consectetur adipiscing elit. Lorem consectetur adipiscing elit."
             Button="View Details"
           />
@@ -29,7 +30,7 @@ const Listings = () => {
         </div>
       </div>
     </section>
-  </>
+  </div>
 
 }
 
