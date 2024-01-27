c1dce657d4a7198a6d6aa7dc71f5e59054cbb96c
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-27 23:10:40 +0000

    Create a mockup for reset

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -67,7 +67,7 @@ export default function SignInPage() {
                   </div>
                 </div>
                 <a
-                  href="#"
+                  href="/reset"
                   className="text-sm font-medium text-orange-600 hover:underline dark:text-primary-500"
                 >
                   Forgot password?
@@ -77,16 +77,12 @@ export default function SignInPage() {
                 type="submit"
                 className="w-full text-white bg-orange-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                <button
-                  type="submit"
-                  className="w-full text-white bg-orange-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                ></button>
                 <FaGoogle /> Login with google
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
                 <a
-                  href="#"
+                  href="/register"
                   className="font-medium text-primary-600 hover:underline  text-orange-600 bg-primary-600"
                 >
                   Sign up
--- src/app/reset/page.js
+++ src/app/reset/page.js
@@ -8,7 +8,7 @@ export default function SignInPage() {
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign Up
+              Reset
             </h1>
 
             <form className="space-y-4 md:space-y-6" action="#">
@@ -28,52 +28,31 @@ export default function SignInPage() {
                   required=""
                 />
               </div>
-              <div>
-                <label
-                  for="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-                <label
-                  for="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Confirm Password
-                </label>
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  id="confirmPassword"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
 
               <button
                 type="submit"
                 className="w-full text-white bg-orange-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Sign up
+                Reset Password
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account?{" "}
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="ml-3 text-sm">
+                    <a
+                      href="/login"
+                      className="text-gray-500 dark:text-gray-300"
+                    >
+                      Sign In
+                    </a>
+                  </div>
+                </div>
                 <a
-                  href="/login"
-                  className="font-medium text-primary-600 hover:underline  text-orange-600 bg-primary-600"
+                  href="/register"
+                  className="text-sm font-medium text-orange-600 hover:underline dark:text-primary-500"
                 >
-                  Sign in
+                  Sign Up
                 </a>
-              </p>
+              </div>
             </form>
           </div>
         </div>
