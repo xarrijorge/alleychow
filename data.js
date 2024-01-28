e6680cf9dc7ee7726477d9c0f1af4112dee8ae77
Xarri Jorge <kodinglife@gmail.com>
2024-01-28 14:42:20 +0000

    Added dummy data

---
+++ data.js
@@ -0,0 +1,64 @@
+const restaurants = [
+    {
+        "id": 1,
+        "name": "Spicy Jollof Haven",
+        "cuisine": "Ghanian",
+        "rating": 4.5,
+        "location": {
+            "latitude": 5.6037,
+            "longitude": -0.1870
+        },
+        "address": "123 Main Street, Accra",
+        "image": "https://example.com/spicy_jollof.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Suya Delights",
+        "cuisine": "Ghanian",
+        "rating": 4.2,
+        "location": {
+            "latitude": 5.5600,
+            "longitude": -0.2050
+        },
+        "address": "456 Cocoa Avenue, Kumasi",
+        "image": "https://example.com/suya_delights.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Banku Bliss",
+        "cuisine": "Ghanian",
+        "rating": 4.0,
+        "location": {
+            "latitude": 5.5575,
+            "longitude": -0.2004
+        },
+        "address": "789 Plantain Road, Tamale",
+        "image": "https://example.com/banku_bliss.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Fufu Fusion",
+        "cuisine": "Ghanian",
+        "rating": 4.8,
+        "location": {
+            "latitude": 5.7980,
+            "longitude": -0.2182
+        },
+        "address": "101 Palm Street, Sekondi-Takoradi",
+        "image": "https://example.com/fufu_fusion.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Waakye Wonders",
+        "cuisine": "Ghanian",
+        "rating": 4.3,
+        "location": {
+            "latitude": 5.5529,
+            "longitude": -0.2123
+        },
+        "address": "202 Baobab Lane, Cape Coast",
+        "image": "https://example.com/waakye_wonders.jpg"
+    }
+]
+
+export default restaurants
