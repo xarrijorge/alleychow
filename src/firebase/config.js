bdc26c3840fb1209c9a522e139f9f7a84b1b7eed
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-29 20:55:23 +0000

    safe the keys to env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCZQl0Q9C1ChisV7nFkC24BmFmivJ0Bjzs",
-  authDomain: "alleychow-b92f7.firebaseapp.com",
-  projectId: "alleychow-b92f7",
-  storageBucket: "alleychow-b92f7.appspot.com",
-  messagingSenderId: "981822913296",
-  appId: "1:981822913296:web:2eb78157bc9f6e28fa8395",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
