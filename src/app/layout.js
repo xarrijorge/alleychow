ccc1cbc38ba3cbda8c703be7081c9256899cbdf7
francoismavunila <mavunila002@gmail.com>
2024-02-02 09:20:21 +0000

    added dynamic data to the message

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import tailwindConfig from "../../tailwind.config";
-import './globals.css'
-import Footer from './components/Footer'
-import Navigation from './components/Navigation'
+import "./globals.css";
+import Footer from "./Components/Footer";
+import Navigation from "./Components/Navigation";
+// import { Router } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +17,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <><body>
-      <>
-        <Navigation />
+    <html lang="en">
+      <body className={inter.className}>
+        <div id="loader"></div>
+        <Navigation/>
         {children}
         <Footer />
-      </>
-    </body>
-    </>
-  )
+      </body>
+    </html>
+  );
 }
--- src/app/listing/page.js
+++ src/app/listing/page.js
@@ -1,8 +1,10 @@
 "use client";
-import React from "react";
+import React, { useState , useEffect} from "react";
 import restaurants from "../../../data";
 import { useSearchParams } from "next/navigation";
 import axios from "axios";
+import { auth } from "@/firebase/config";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 const Listing = () => {
   const searchParams = useSearchParams();
@@ -11,6 +13,27 @@ const Listing = () => {
   const URI = "https://graph.facebook.com/v19.0/225937017268827/messages"
 
   const accessToken = process.env.NEXT_PUBLIC_WHATSAPP_ACCESS_TOKEN;
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [description,setDescription] = useState("")
+  const [displayUsername, setDisplayUsername] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [position, setPosition] = useState(null);
+
+  const  getLocation =  ()=>{
+    if (!navigator.geolocation) {
+      alert('Geolocation is not supported by your browser');
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      setPosition({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    }, () => {
+      alert('Unable to retrieve your location');
+    });
+  }
 
   const data = {
     messaging_product: 'whatsapp',
@@ -19,17 +42,29 @@ const Listing = () => {
     type: 'text',
     text: {
       preview_url: false,
-      body: 'Hello from the frontend',
+      body: `Order from ${displayUsername}\nfor ${restaurant.name}\nat ${restaurant.address}\nfor $40.\n${phoneNumber? 'Contact at '+phoneNumber : 'No contact information'}\nDescription : ${description}\nLocation :  href=https://www.google.com/maps/search/?api=1&query=${position?.latitude},${position?.longitude}`,
     },
   };
+
+  const validatePhoneNumber = (number) => {
+    const pattern = /^\+\d{1,3}\d{9}$/;
+    return pattern.test(number);
+   };
+   
 
   const headers = {
     Authorization: `Bearer ${accessToken}`,
     'Content-Type': 'application/json',
   };
 
+  useEffect(() => {
+    setIsValid(validatePhoneNumber(phoneNumber));
+   }, [phoneNumber]);
 
-  const orderNow = () => {
+  const orderNow =  () => {
+    // getLocation()
+    console.log(displayUsername)
+    console.log(data.text.body)
     axios.post(URI, data, { headers })
       .then(response => {
         console.log('Message sent successfully:', response.data);
@@ -37,59 +72,84 @@ const Listing = () => {
       .catch(error => {
         console.error('Error sending message:', error.response.data);
       });
-    // console.log('testing')
+    console.log('testing')
   }
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setDisplayUsername(user.displayName);
+      }
+    });
+  }, []);
 
   return (
     <>
-      <div class="py-8">
-        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="flex flex-col md:flex-row -mx-4">
-            <div class="md:flex-1 px-4">
-              <div class="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+      <div className="py-8">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-col md:flex-row -mx-4">
+            <div className="md:flex-1 px-4">
+              <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
                 <img
-                  class="w-full h-full object-cover"
+                  className="w-full h-full object-cover"
                   src={restaurant.image}
                   alt="Product Image"
                 />
               </div>
-              <div class="flex -mx-2 mb-4">
-                <div class="w-1/2 px-2">
-                  <button onClick={orderNow} class="w-full bg-orange-700 dark:bg-orange-600 text-white py-2 px-4 rounded-full font-bold hover:bg-orange-400 dark:hover:bg-orange-700">
+              <div className="flex -mx-2 mb-4">
+                <div className="w-1/2 px-2">
+                  <form>
+                    <label>Your Contact Number</label>
+                    <input
+                      type="text"
+                      value={phoneNumber}
+                      placeholder="Provide Your Phone number"
+                      onChange={(e)=>setPhoneNumber(e.target.value)}
+                      className="w-full md:w-6/10 lg:w-6/10 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+                    />
+                    <label>Description</label>
+                    <textarea
+                      id="description"
+                      value={description}
+                      onChange={(e)=>setDescription(e.currentTarget.value)}
+                      placeholder="Enter your description here..."
+                      className="w-full md:w-6/10 lg:w-6/10 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+                    />
+                  </form>
+                  <button onClick={orderNow} className="w-full bg-orange-700 dark:bg-orange-600 text-white py-2 px-4 rounded-full font-bold hover:bg-orange-400 dark:hover:bg-orange-700" disabled={!isValid}>
                     Order Now
                   </button>
                 </div>
               </div>
             </div>
-            <div class="md:flex-1 px-4">
-              <h2 class="text-2xl font-bold  dark:text-gray-800 mb-2">
+            <div className="md:flex-1 px-4">
+              <h2 className="text-2xl font-bold  dark:text-gray-800 mb-2">
                 {restaurant.name}
               </h2>
-              <p class="text-gray-900 dark:text-gray-900 text-sm mb-4">
+              <p className="text-gray-900 dark:text-gray-900 text-sm mb-4">
                 {restaurant.address}
               </p>
-              <div class="flex mb-4">
-                <div class="mr-4">
-                  <span class="font-bold text-gray-700 dark:text-gray-800">
+              <div className="flex mb-4">
+                <div className="mr-4">
+                  <span className="font-bold text-gray-700 dark:text-gray-800">
                     Price:
                   </span>
-                  <span class="text-gray-600 dark:text-gray-800">$29.99</span>
+                  <span className="text-gray-600 dark:text-gray-800">$29.99</span>
                 </div>
                 <div>
-                  <span class="font-bold text-gray-700 dark:text-gray-800">
+                  <span className="font-bold text-gray-700 dark:text-gray-800">
                     Availability:
                   </span>
-                  <span class="text-gray-600 dark:text-gray-800">
+                  <span className="text-gray-600 dark:text-gray-800">
                     Available
                   </span>
                 </div>
               </div>
 
               <div>
-                <span class="font-bold text-gray-700 dark:text-gray-800">
+                <span className="font-bold text-gray-700 dark:text-gray-800">
                   Food Details:
                 </span>
-                <p class="text-gray-800 dark:text-gray-800 text-sm mt-2">
+                <p className="text-gray-800 dark:text-gray-800 text-sm mt-2">
                   {restaurant.description}
                 </p>
               </div>
