34af399cffebd40ce2cd2f0a058edd93445d94b6
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-02-02 12:56:23 +0000

    Adjust the food listing image

--- src/app/Components/Card.jsx
+++ src/app/Components/Card.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const SingleCard = ({
   image,
@@ -11,29 +11,33 @@ const SingleCard = ({
 }) => {
   return (
     <>
-      <div className='mb-10 overflow-hidden rounded-lg bg-white shadow-1 duration-300 hover:shadow-3 dark:bg-dark-2 dark:shadow-card dark:hover:shadow-3'>
-        <img src={image} alt='' className='w-full' />
-        <div className='p-8 text-center sm:p-9 md:p-7 xl:p-9'>
+      <div className="mb-10 overflow-hidden rounded-lg border border-red-500 bg-white shadow-1 duration-300 hover:shadow-3 dark:bg-dark-2 dark:shadow-card dark:hover:shadow-3">
+        <div className=" h-[300px] overflow-hidden">
+          <img src={image} alt="" className="w-full h-full" />
+        </div>
+        <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
           <h3>
             <a
-              href={titleHref ? titleHref : '/#'}
-              className=' mb-4 block text-xl font-semibold text-dark hover:text-primary  sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]'>
+              href={titleHref ? titleHref : "/#"}
+              className=" mb-4 block text-xl font-semibold text-dark hover:text-primary  sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+            >
               {CardTitle}
             </a>
           </h3>
-          <p className='mb-7 text-base leading-relaxed text-body-color dark:text-dark-6'>
+          <p className="mb-7 text-base leading-relaxed text-body-color dark:text-dark-6">
             {CardDescription}
           </p>
 
           {Button && (
             <Link
               href={{
-                pathname: '/listing',
+                pathname: "/listing",
                 query: {
                   id,
                 },
               }}
-              className='inline-block rounded-full border border-gray-3 px-7 py-2 text-base font-medium text-body-color transition hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-dark-6'>
+              className="inline-block rounded-full border border-gray-3 px-7 py-2 text-base font-medium text-body-color transition hover:border-primary hover:bg-primary hover:text-white dark:border-dark-3 dark:text-dark-6"
+            >
               {Button}
             </Link>
           )}
@@ -41,7 +45,7 @@ const SingleCard = ({
       </div>
       {/*  */}
     </>
-  )
-}
+  );
+};
 
-export default SingleCard
+export default SingleCard;
